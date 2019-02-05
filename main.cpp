773e7c2c6e22ee4c80c66826e08f73b939ac9aa8
Eiji Mitsumizo <eiji.mitsumizo-1@ou.edu>
2019-02-04 19:03:22 -0600

    It works and cleaned up but looking for more efficient algolithm

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 class SparseRow {
@@ -11,10 +10,10 @@ protected:
 public:
     SparseRow();
     void display();
-    void setSparseRow(int, int, int); //(row, col, value)
-    void setRow(int);
-    void setCol(int);
-    void setVal(int);
+    void setSparseRow(int r, int c, int val);
+    void setRow(int r); //
+    void setCol(int c);
+    void setVal(int val);
     int getRow();
     int getCol();
     int getValue();
@@ -29,20 +28,20 @@ protected:
     SparseRow* myMatrix; //Array consisted of SparseRow : [SparseRow, SparseRow, ....] the size is by the number of noSparseValues
 
 public:
-    SparseMatrix(); //I do not know
+    SparseMatrix();
     SparseMatrix(int n, int m, int cv, int noNSV);
-    SparseMatrix(SparseMatrix& P);
     SparseMatrix* Transpose();
     SparseMatrix* Multiply(SparseMatrix& M);
     SparseMatrix* Add(SparseMatrix& M);
     void display();
     void displayMatrix();
+
     int getRow();
     int getCol();
     int getCV();
     int getNoNSV();
     SparseRow* getMyMatrix();
-    void setSparseRow(int, int, int, int);
+    void setSparseRow(int r, int c, int val, int count);
     void sorted();
     int detectValue(int row, int col);
 };
@@ -59,30 +58,40 @@ void SparseRow::display()
     cout << getRow() << ", " << getCol() << ", " << getValue() << endl;
 }
 
-void SparseRow::setSparseRow(int n, int m, int cv)
+void SparseRow::setSparseRow(int r, int c, int val)
 {
-    row = n;
-    col = m;
-    value = cv;
+    row = r;
+    col = c;
+    value = val;
 }
 
-void SparseRow::setRow(int r) {
+void SparseRow::setRow(int r)
+{
     row = r;
 }
-void SparseRow::setCol(int c){
+
+void SparseRow::setCol(int c)
+{
     col = c;
 }
-void SparseRow::setVal(int v) {
-    value = v;
+
+void SparseRow::setVal(int val)
+{
+    value = val;
 }
 
-int SparseRow::getRow(){
+int SparseRow::getRow()
+{
     return row;
 }
-int SparseRow::getCol(){
+
+int SparseRow::getCol()
+{
     return col;
 }
-int SparseRow::getValue(){
+
+int SparseRow::getValue()
+{
     return value;
 }
 
@@ -94,6 +103,7 @@ SparseMatrix::SparseMatrix()
     noNonSparseValues = 0;
     myMatrix = new SparseRow[noNonSparseValues]; //array
 }
+
 SparseMatrix::SparseMatrix (int n, int m, int cv, int noNSV)
 {
     noRows = n;
@@ -103,30 +113,26 @@ SparseMatrix::SparseMatrix (int n, int m, int cv, int noNSV)
     myMatrix = new SparseRow[noNSV];
 }
 
-SparseMatrix::SparseMatrix(SparseMatrix &P) {
-    noRows = P.getRow();
-    noCols = P.getCol();
-    commonValue = P.getCV();
-    noNonSparseValues = P.getNoNSV();
-    myMatrix = new SparseRow (*P.getMyMatrix());
-}
-
 int SparseMatrix::getRow()
 {
     return noRows;
 }
+
 int SparseMatrix::getCol()
 {
     return noCols;
 }
+
 int SparseMatrix::getCV()
 {
     return commonValue;
 }
+
 int SparseMatrix::getNoNSV()
 {
     return noNonSparseValues;
 }
+
 SparseRow* SparseMatrix::getMyMatrix()
 {
     return myMatrix;
@@ -134,14 +140,16 @@ SparseRow* SparseMatrix::getMyMatrix()
 
 void SparseMatrix::setSparseRow(int row , int col , int value, int count)
 {
-    getMyMatrix()[count].setSparseRow(row, col, value);
+    myMatrix[count].setSparseRow(row, col, value); // insert the SparseRow into the myMatrix by count
 }
 
 void SparseMatrix::display()
 {
-    for (int i = 0; i < getNoNSV(); i++) {
-        if(getMyMatrix()[i].getValue() != getCV()) { //need to update method and sort method
-            getMyMatrix()[i].display();
+    for (int i = 0; i < noNonSparseValues; i++)
+    {
+        if(myMatrix[i].getValue() != commonValue)//need to update method and sort method
+        {
+            myMatrix[i].display();
         }
     }
 }
@@ -149,23 +157,22 @@ void SparseMatrix::display()
 void SparseMatrix::displayMatrix()
 {
     int count = 0;
-    while(getMyMatrix()[count].getRow() < 0)
+    while(myMatrix[count].getRow() < 0)
     {
         count++;
     }
-    for(int i = 0; i < getRow(); i++)
+    for(int i = 0; i < noRows; i++)
     {
-        for(int j = 0; j < getCol(); j++)
+        for(int j = 0; j < noCols; j++)
         {
-
-            if(getMyMatrix()[count].getRow() == i && getMyMatrix()[count].getCol() == j) //this works if it is ordered
+            if(myMatrix[count].getRow() == i && myMatrix[count].getCol() == j) //this works if it is ordered
             {
-                cout << getMyMatrix()[count].getValue() << "\t";
+                cout << myMatrix[count].getValue() << "\t";
                 count++;
             }
             else
             {
-                cout << getCV() << "\t";
+                cout << commonValue << "\t";
             }
         }
         cout << endl;
@@ -174,9 +181,9 @@ void SparseMatrix::displayMatrix()
 
 SparseMatrix* SparseMatrix::Transpose()
 {
-    SparseMatrix* transposed = new SparseMatrix(getCol(), getRow(), getCV(), getNoNSV());
+    SparseMatrix* transposed = new SparseMatrix(noCols, noRows, commonValue, noNonSparseValues);
 
-    for(int i = 0; i < getNoNSV(); i++)
+    for(int i = 0; i < noNonSparseValues; i++)
     {
         (*transposed).myMatrix[i].setSparseRow(myMatrix[i].getCol(), myMatrix[i].getRow(), myMatrix[i].getValue());
     }
@@ -188,31 +195,31 @@ SparseMatrix* SparseMatrix::Transpose()
 
 SparseMatrix* SparseMatrix::Add(SparseMatrix &M)
 {
-    SparseMatrix* added = new SparseMatrix(getRow(), getCol(), getCV(), getNoNSV() + M.getNoNSV());
+    SparseMatrix* added = new SparseMatrix(noRows, noCols, commonValue, noNonSparseValues + M.noNonSparseValues);
 
     int currentSize = 0;
     int i = 0;
     int j;
     bool found;
-    bool* isBox = new bool[M.getNoNSV()];
+    bool* isBox = new bool[M.noNonSparseValues];
 
-    for(j = 0; j < M.getNoNSV(); j++)
+    for(j = 0; j < M.noNonSparseValues; j++)
     {
         isBox[j] = false;
     }
 
-    while (i < getNoNSV())
+    while (i < noNonSparseValues)
     {
         found = false;
-        (*added).getMyMatrix()[currentSize].setRow(getMyMatrix()[i].getRow());
-        (*added).getMyMatrix()[currentSize].setCol(getMyMatrix()[i].getCol());
+        (*added).myMatrix[currentSize].setRow(myMatrix[i].getRow());
+        (*added).myMatrix[currentSize].setCol(myMatrix[i].getCol());
 
-        for(j = 0; j < M.getNoNSV(); j++)
+        for(j = 0; j < M.noNonSparseValues; j++)
         {
-            if((getMyMatrix()[i].getRow() == M.getMyMatrix()[j].getRow()) &&
-                    (getMyMatrix()[i].getCol() == M.getMyMatrix()[j].getCol()))
+            if((myMatrix[i].getRow() == M.myMatrix[j].getRow()) &&
+                    (myMatrix[i].getCol() == M.myMatrix[j].getCol()))
             {
-                (*added).getMyMatrix()[currentSize++].setVal(getMyMatrix()[i].getValue() + M.getMyMatrix()[j].getValue());
+                (*added).myMatrix[currentSize++].setVal(myMatrix[i].getValue() + M.myMatrix[j].getValue());
                 found = true;
                 isBox[j] = found;
                 break;
@@ -220,17 +227,17 @@ SparseMatrix* SparseMatrix::Add(SparseMatrix &M)
         }
         if(!found)
         {
-            (*added).getMyMatrix()[currentSize++].setVal((getMyMatrix()[i].getValue()));
+            (*added).myMatrix[currentSize++].setVal((myMatrix[i].getValue()));
         }
         i++;
     }
 
     i = 0;
-    while(i < M.getNoNSV())
+    while(i < M.noNonSparseValues)
     {
         if(!isBox[i])
         {
-            (*added).getMyMatrix()[currentSize++].setSparseRow(M.getMyMatrix()[i].getRow(),M.getMyMatrix()[i].getCol(),M.getMyMatrix()[i].getValue());
+            (*added).myMatrix[currentSize++].setSparseRow(M.myMatrix[i].getRow(), M.myMatrix[i].getCol(), M.myMatrix[i].getValue());
         }
         i++;
     }
@@ -243,101 +250,97 @@ SparseMatrix* SparseMatrix::Add(SparseMatrix &M)
 
 int SparseMatrix::detectValue(int row, int col)
 {
-    for(int i = 0; i < getNoNSV(); i++)
+    for(int i = 0; i < noNonSparseValues; i++)
     {
-        if(getMyMatrix()[i].getRow() == row && getMyMatrix()[i].getCol() == col)
+        if(myMatrix[i].getRow() == row && myMatrix[i].getCol() == col)
         {
-            return getMyMatrix()[i].getValue();
+            return myMatrix[i].getValue();
         }
     }
-
     return -1;
 }
+
 SparseMatrix* SparseMatrix::Multiply(SparseMatrix &M) {
 
-    SparseMatrix* Multiplied = new SparseMatrix(getRow(), getCol(), getCV(), getRow() * getCol());
+    SparseMatrix* Multiplied = new SparseMatrix(noRows, noCols, commonValue, noRows * noCols);
 
     int count = 0;
     int sum;
-    int aval;
-    int bval;
+    int a;
+    int b;
 
-    for(int i = 0; i < getRow(); i++)
+    for(int i = 0; i < noRows; i++)
     {
-        for(int j = 0; j < M.getCol(); j++)
+        for(int j = 0; j < M.noCols; j++)
         {
             sum = 0;
-            for(int k = 0; k < getRow(); k++)
+            for(int k = 0; k < noRows; k++)
             {
-                aval = detectValue(i, k);
-                bval = M.detectValue(k, j);
+                a = detectValue(i, k);
+                b = M.detectValue(k, j);
 
-                if(aval != -1 && bval != -1)
+                if(a != -1 && b != -1)
                 {
-                   sum += aval * bval;
+                   sum += a * b;
                 }
             }
-            if(sum != getCV())
+            if(sum != commonValue)
             {
-                Multiplied->setSparseRow(i, j, sum, count++);
+                (*Multiplied).setSparseRow(i, j, sum, count++);
             }
         }
     }
-
     (*Multiplied).sorted();
 
     return Multiplied;
-
 }
 
 void SparseMatrix::sorted()
 {
 
-    SparseRow amatrix;
     SparseRow tmp;
 
-    for(int i = 0; i < getNoNSV() - 1; i++)
+    for(int i = 0; i < noNonSparseValues - 1; i++)
     {
-        for(int j = 0; j < getNoNSV() - i - 1; j++)
+        for(int j = 0; j < noNonSparseValues - i - 1; j++)
         {
-            if(getMyMatrix()[j].getRow() > getMyMatrix()[j + 1].getRow())
+            if(myMatrix[j].getRow() > myMatrix[j + 1].getRow())
             {
-                tmp = getMyMatrix()[j];
-                getMyMatrix()[j] = getMyMatrix()[j + 1];
-                getMyMatrix()[j + 1] = tmp;
+                tmp = myMatrix[j];
+                myMatrix[j] = myMatrix[j + 1];
+                myMatrix[j + 1] = tmp;
             }
         }
     }
 
-    for(int i = 0; i < getNoNSV() - 1; i++)
+    for(int i = 0; i < noNonSparseValues - 1; i++)
     {
-        for(int j = 0; j < getNoNSV() - i - 1; j++)
+        for(int j = 0; j < noNonSparseValues- i - 1; j++)
         {
-            if(getMyMatrix()[j].getCol() > getMyMatrix()[j + 1].getCol() && getMyMatrix()[j].getRow() == getMyMatrix()[j + 1].getRow())
+            if(myMatrix[j].getCol() > myMatrix[j + 1].getCol() && myMatrix[j].getRow() == myMatrix[j + 1].getRow())
             {
-                tmp = getMyMatrix()[j];
-                getMyMatrix()[j] = getMyMatrix()[j + 1];
-                getMyMatrix()[j + 1] = tmp;
+                tmp = myMatrix[j];
+                myMatrix[j] = myMatrix[j + 1];
+                myMatrix[j + 1] = tmp;
             }
         }
     }
 }
+
 int main()
 {
-
-    int n, m, cv, noNSV;
+    int row, col, cv, noNSV;
     SparseMatrix* temp;
 
-    cin >> n >> m >> cv >> noNSV;
+    cin >> row >> col >> cv >> noNSV;
 
-   SparseMatrix* firstOne = new SparseMatrix(n, m, cv, noNSV);
-
+   SparseMatrix* firstOne = new SparseMatrix(row, col, cv, noNSV);
 
    int value;
    int count = 0;
-   for(int i = 0; i < n; i++)
+   for(int i = 0; i < row; i++)
    {
-       for(int j = 0; j < m; j++)
+       for(int j = 0; j < col; j++)
        {
            cin  >> value;
            if(value != cv)
@@ -345,18 +348,17 @@ int main()
                (*firstOne).setSparseRow(i, j, value, count);
                count++;
            }
-
        }
    }
 
-    cin >> n >> m >> cv >> noNSV;
+    cin >> row >> col >> cv >> noNSV;
 
-    SparseMatrix* secondOne = new SparseMatrix(n, m, cv, noNSV);
+    SparseMatrix* secondOne = new SparseMatrix(row, col, cv, noNSV);
 
     count = 0;
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < row; i++)
     {
-        for(int j = 0; j < m; j++)
+        for(int j = 0; j < col; j++)
         {
             cin  >> value;
             if(value != cv) {
@@ -389,13 +391,12 @@ int main()
     (*temp).displayMatrix();
 
     cout << "multiplication of matrices in sparse matrix form: " << endl;
-    temp = (*firstOne).Multiply(*secondOne);
-    (*temp).displayMatrix();
+    temp = (*secondOne).Multiply(*firstOne);
+    (*temp).display();
 
     cout << "addition of matrices in sparse matrix form: " << endl;
     temp = (*secondOne).Add(*firstOne);
-
-    (*temp).displayMatrix();
+    (*temp).display();
 
     return 0;
 }
